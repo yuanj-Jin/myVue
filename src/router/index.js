37c00da678c1439bc23e5f0d9eccb53c64ed44f3
yuanj1 <yuanj1@corp.21cn.com>
2019-12-31 17:45:24 +0800

    develop

--- src/router/index.js
+++ src/router/index.js
@@ -2,6 +2,11 @@ import Vue from 'vue'
 import Router from 'vue-router'
 import Login from '../components/Login'
 import App from '@/App'
+import ElementUi from 'element-ui'
+import 'element-ui/lib/theme-chalk/index.css'
+
+// import '@/theme-et/index.css'
+Vue.use(ElementUi)
 
 
 Vue.use(Router)
