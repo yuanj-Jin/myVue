e8b43333b1e3c3fb4459da55f6f54d81d68b02c0
yuanj1 <yuanj1@corp.21cn.com>
2020-01-08 17:52:31 +0800

    manager page

---
+++ src/components/system/Role.vue
@@ -0,0 +1,42 @@
+<template>
+  <el-main>
+    <el-table :data="tableData">
+      <el-table-column prop="id" label="id" width="140"></el-table-column>
+      <el-table-column prop="date" label="日期" width="140"></el-table-column>
+      <el-table-column prop="name" label="角色" width="120"></el-table-column>
+      <el-table-column prop="address" label="角色名称"></el-table-column>
+      <el-table-column align="right">
+        <template slot="header" slot-scope="scope">
+          <el-input v-model="search" size="mini" placeholder="输入关键字搜索" />
+        </template>
+        <template slot-scope="scope">
+          <el-button size="mini" @click="handleEdit(scope.$index, scope.row)">Edit</el-button>
+          <el-button size="mini" type="danger" @click="handleDelete(scope.$index, scope.row)">Delete</el-button>
+        </template>
+      </el-table-column>
+    </el-table>
+  </el-main>
+</template>
+
+<script>
+export default {
+  data() {
+    const i=0;
+    const item = {
+      id: 1,
+      date: "2016-05-02",
+      name: "王小虎",
+      address: "上海市普陀区金沙江路 1518 弄"
+    };
+    return {
+      tableData: Array(20).fill(item),
+      isCollapse: true,
+      search: ""
+    };
+  }
+};
+</script>
+
+<style>
+
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,11 +1,13 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import Manager from '../components/system/Manager'
-import Home from '../components/system/Home'
+import Home from '../components/Home'
 import App from '@/App'
 import ElementUi from 'element-ui'
 import 'element-ui/lib/theme-chalk/index.css'
 
+import Role from '../components/system/Role'
+import User from '../components/system/User'
 // import '@/theme-et/index.css'
 Vue.use(ElementUi)
 
@@ -36,6 +38,18 @@ export default new Router({
       path: '/home',
       name: 'home',
       component: Home
+    }
+    ,
+    {
+      path: '/4-1',
+      name: '4-1',
+      component: User
+    }
+    ,
+    {
+      path: '/4-2',
+      name: '4-2',
+      component: Role
     }
 
   ]
